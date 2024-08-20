c3dd6cedf8b7354e5e9684afae962d2df97287f5
NightDev83 <dylan.bouraoui83@gmail.com>
2024-08-20 22:40:20 +0200

    push FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Faq from "./pages/Faq";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/fAQ" Component={Faq} />
       </Routes>
     </Router>
   );
