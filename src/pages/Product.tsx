99d4d5f94f75b6cc36dcc39d7cfb37989917e49e
Dark-Soleil <adonai.ouisol@epitech.eu>
2024-09-03 13:55:31 +0200

    Fix push

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -26,7 +26,7 @@ import {
 } from "../Requests/WishlistRequest";
 import { CreateWishlistBody, UpdateWishlistBody } from "../Types/wishlist";
 import { useConnected } from "../hooks/Connected";
-import { getWishListItems, putWishList } from "../hooks/wishListe";
+import { getWishListItems } from "../hooks/wishListe";
 
 export default function Product() {
   const { category, productTitle, id } = useParams();
